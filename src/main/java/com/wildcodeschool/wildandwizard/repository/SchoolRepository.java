dee107f0d26ec37bec7b2fa3d43193db23df24e2
klatysheva <latysheva.ks@yandex.ru>
2021-06-23 20:36:17 +0300

    Quest 417: School functionality added

---
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -0,0 +1,7 @@
+package com.wildcodeschool.wildandwizard.repository;
+
+import com.wildcodeschool.wildandwizard.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
